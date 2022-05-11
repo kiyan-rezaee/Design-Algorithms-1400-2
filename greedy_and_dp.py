97c77b42aa15ea8b8333cf6446ea7430b06a6b43
kiyan rezaee <kiyanrezaee176@gmail.com>
2022-05-11 23:59:04 +0430

    Final Commit

--- greedy_and_dp.py
+++ greedy_and_dp.py
@@ -7,19 +7,12 @@ n = len(p)
 
 def dp(n, m, p):
     dp = [[0 for _ in range(n)] for _ in range(m)]
-    dp[0][0] = p[0]
     for i in range(m):
         for j in range(n):
-            if i == 0 and j == 0:
-                pass
-            elif i == 0:
-                dp[i][j] = dp[i][j - 1] + sum(p[:j + 1])
+            if i == j:
+                dp[i][j] = dp[i][j - 1] + p[j]
             elif i > j:
                 dp[i][j] = dp[i - 1][j]
-            elif i == j:
-                dp[i][j] = dp[i][j - 1] + p[i]
-            elif i == 1:
-                dp[i][j] = dp[i - 1][j] - dp[i][j - 1]
             else:
                 dp[i][j] = dp[i][j - 1] + (dp[i][j - i - 1] -
                                            dp[i][j - i - 2]) + p[j]
@@ -42,6 +35,6 @@ for i in range(400):
     n = len(p)
     a = greedy(n, m, p)
     b = dp(n, m, p)
-    print(a,b)
+    # print(a,b)
     if a != b:
         print('false',a, b)
